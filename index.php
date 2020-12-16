51bacb593b0b6d94af45647adc6f9a87fd7237b0
cherubina-pantano <cherubina.pantano@gmail.com>
2020-12-16 15:59:57 +0100

    Header

--- index.php
+++ index.php
@@ -3,9 +3,38 @@
   <head>
     <meta charset="utf-8">
     <title>Google FAQ</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="./css/style.css">
   </head>
   <body>
+    <!-- Header -->
+    <header class="d-flex">
+      <div class="header-top d-flex">
+        <section class="header-sx d-flex">
+          <img src="./img/google-logo.png" alt="logo">
+          <h3>Privacy e termini</h3>
+        </section>
+        <section class="header-dx d-flex">
+          <i class="fas fa-th"></i>
+          <i class="fas fa-user-circle"></i>
+        </section>
+      </div>
+      
+      <!-- Nav -->
+      <nav class="main-navbar d-flex">
+        <ul class="navbar d-flex">
+          <li> <a href="#"> Introduzione </a> </li>
+          <li> <a href="#"> Norme sulla privacy </a> </li>
+          <li> <a href="#"> Termini di servizio </a> </li>
+          <li> <a href="#"> Tecnologie </a> </li>
+          <li> <a href="#"> Domande frequenti </a> </li>
+        </ul>
+      </nav>
+
+    </header>
+
 
   </body>
 </html>
